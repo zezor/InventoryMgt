9cf2ca0036a68502619d2811c85e2e3d2acb2f16
zezor <kemma2993@gmail.com>
2025-08-14 19:02:15 -0800

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from .models import CustomUser
 
+
+
+User = get_user_model()
+
 # Create your views here.
 def login_view(request):
-    form = CustomUser()
+    form = get_user_model()  # Assuming you have a form for login
+    
     return render(request, 'accounts/login.html', {'form': form})
